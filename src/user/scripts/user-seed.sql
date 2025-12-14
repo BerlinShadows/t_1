9cebea64f7953b7ce2d93015be21007f99881c80
Mikhail Landau <mi-landau@outlook.com>
2025-12-14 23:45:43 +0300

    feat: sql seed for tests

--- src/user/scripts/user-seed.sql
+++ src/user/scripts/user-seed.sql
@@ -1,4 +1,11 @@
+---- TEST SQL SEED ----
+
 INSERT INTO
-    public."user" (id, balance)
+    "user" (id, balance)
 VALUES
-    (default, 1000);
+    (default, 100);
+
+INSERT INTO
+    "ledger" (id, action, amount, status, user_id)
+VALUES
+    (default, 'deposit', 100, 'completed', 1);
